793dc6b1634640af79f07d08d4311f74217195a6
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-10-13 14:38:35 -0300

    js finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const active = (element) => {
+  //CLICK// /*
+  let itens = document.getElementsByClassName("btns-circle");
+
+  for (i = 0; i < itens.length; i++) {
+    itens[i].classList.remove("active");
+  }
+  element.classList.add("active");
+};
+
+const box = document.querySelector("#box");
+const box1 = document.querySelector("#box1");
+const btnSubmit = document.querySelector(".btn-submit");
+
+btnSubmit.addEventListener("click", () => {
+  const circleSelect = [...document.querySelectorAll(".active")];
+  const numSelect = document.querySelector(".num-select");
+
+  box.style.display = "none";
+  box1.style.display = "block";
+
+  circleSelect.map((element) => {
+    numSelect.innerHTML = element.id;
+  });
+});
